978f15d45b0af460f358a05921b1a691038f41e5
Stan <stan_ts@hotmail.com>
2016-03-05 01:56:23 +1030

    added delay after sending initialisation

--- SerialTest.py
+++ SerialTest.py
@@ -4,14 +4,20 @@ import os, sys
 import serial
 import time
 
-PORT = serial.Serial("/dev/FIND_THIS_BIT", 9600, timeout=None)
-initConsult()
 
 
 def initConsult():
     PORT.flushInput()
     PORT.write("\xFF\xFF\xEF")
+    time.sleep(2)
     if PORT.read(1) == "\x10":
         print("Congratulations: connected to consult successfully")
     else:
         print("unsuccessful connecting to consult")
+
+
+
+PORT = serial.Serial("/dev/FIND_THIS_BIT", 9600, timeout=None)
+initConsult()
+
+
